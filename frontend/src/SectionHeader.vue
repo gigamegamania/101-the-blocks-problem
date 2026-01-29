53588f33bc92a63555265aa402b7491e791b8599
Giga Mega <hello@gigamega.ca>
2026-01-29 09:22:34 -0500

    improvements and bug fixes

---
+++ frontend/src/SectionHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <h2 class="section-header">{{ label }}</h2>
+</template>
+
+<style scoped>
+.section-header {
+  margin-top: 0;
+  margin-bottom: 1rem;
+  color: #42b883;
+}
+</style>
